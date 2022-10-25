65f2051d06b7a1eb23b4b2b9e34340c375ce9392
liuyingdi <ydliu@cnic.cn>
2022-10-25 16:20:50 +0800

    bigdata init

--- src/main/java/com/imooc/hdfs/HDFSTest.java
+++ src/main/java/com/imooc/hdfs/HDFSTest.java
@@ -7,8 +7,8 @@ public class HDFSTest {
     public static void main(String[] args) throws Exception {
 
         HDFSServer hdfs = new HDFSServer();
-        // hdfs.put("./bd_hadoop_resources/files/user.txt", "/user.txt");
-        // hdfs.get("/README.txt", "./bd_hadoop_resources/files/README.txt");
+        hdfs.put("./bd_hadoop_resources/files/user.txt", "/user.txt");
+        hdfs.get("/README.txt", "./bd_hadoop_resources/files/README.txt");
         System.out.println(hdfs.delete("/LICENSE.txt"));
     }
 }
